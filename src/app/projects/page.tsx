cd1a5df4a185012d5b0c709223b198e4f4e1831f
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-06-23 20:37:16 +1200

    wip: project page layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,59 @@
 import { Container } from "@/components/ui/container";
 import { ContentHeader } from "@/components/ui/contentHeader";
+import { Separator } from "@/components/ui/separator";
+import { Typography } from "@/components/ui/typography";
+import { Project, projects } from "@/constants/projects";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
+// import Image from "next/image";
+
+const ProjectCard = ({ project }: { project: Project }) => {
+  return (
+    <div key={project.name} className="rounded-2xl mx-10 my-5 w-96 h-42">
+      <Typography variant="h2" className="my-4">
+        {project.name}
+      </Typography>
+      <ScrollArea className="h-[calc(100%-40px)]">
+        <Typography variant="p">{project.description}</Typography>
+      </ScrollArea>
+      <Separator className="m-2" />
+      <div className="h-20">
+        {project.technologies &&
+          project.technologies?.map((tech) => (
+            <div
+              key={`${project.name}-${tech}`}
+              className="inline-block my-2 mx-1 bg-accent text-slate-700 px-2 rounded-2xl shadow-2xl shadow-slate-700"
+            >
+              <Typography variant="small">{tech}</Typography>
+            </div>
+          ))}
+      </div>
+      <Separator className="m-2" />
+      {project.url && <Link href={project.url} />}
+    </div>
+  );
+};
 
 export default function Projects() {
   return (
     <Container>
       <ContentHeader>Projects</ContentHeader>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="grid grid-cols-1 gap-y-36">
+          {Object.entries(projects).map(([classification, projects]) => (
+            <div key={classification} className="mx-5">
+              <Typography variant="h1" alignment="left" className="">
+                {classification}
+              </Typography>
+              <div className="grid grid-cols-3 my-5">
+                {projects.map((project) => (
+                  <ProjectCard key={project.name} project={project} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </ScrollArea>
     </Container>
   );
 }
---
+++ src/constants/projects.ts
@@ -0,0 +1,62 @@
+type Classification = "Software" | "Other";
+
+export interface Project {
+  name: string;
+  description: string;
+  technologies?: string[];
+  image: string;
+  url?: string;
+}
+
+export const projects: Record<Classification, Project[]> = {
+  Software: [
+    {
+      name: "theqaguy.co.nz",
+      description:
+        "My own personal website to show off um... me and to blog about the world of software quality assurance. If you're on this page, you're probably viewing this project right now.. ",
+      technologies: [
+        "Typescript",
+        "Next.js",
+        "ShadCN",
+        "Vitest",
+        "Playwright",
+        "Tailwind CSS",
+        "Prisma ORM",
+        "PostgreSQL",
+      ],
+      image: "",
+      url: "https://theqaguy.co.nz/",
+    },
+    {
+      name: "MDR Terminal",
+      description:
+        "After binge watching Severance with my wife, I couldn't help but try to build my own spin on the MDR terminal.",
+      technologies: ["Typescript", "React", "Vite", "Tailwind CSS"],
+      image: "/projects/mdr.png",
+      url: "https://macro-data-refinement.theqaguy.co.nz/",
+    },
+    {
+      name: "Massage+ByTheSquare",
+      description:
+        "A local business looking to modernise. After years of running a basic Wix site, this business is wanting to move to fully digital booking management and provide a better experience to customers. This full stack web application provides innovative booking experience for clients and a fully featured admin panel that gives management new reporting capabilities and the ability to more efficiently manage their business. ",
+      technologies: [
+        "Better Auth",
+        "Next.js",
+        "Prisma",
+        "PostgreSQL",
+        "Tailwind CSS",
+        "Playwright",
+      ],
+      image: "",
+      url: "",
+    },
+  ],
+  Other: [
+    {
+      name: "The Open Source Racecar",
+      description:
+        "Building a hobbiest racecar is an expensive endeavour. The most costly part often being engine management. Luckily for me there are some incredibly talented people that have made opensource daughter boards and fimrware for Arduino boards to control engines",
+      image: "",
+    },
+  ],
+};
