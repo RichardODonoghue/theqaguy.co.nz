2c5f2a999dbf466c27ca8e45a6aef0eb78639d77
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-07-03 17:52:16 +1200

    fix: aboutme scroll area on zfold

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -97,7 +97,7 @@ export default function AboutMe() {
   return (
     <>
       <ContentHeader>AboutMe</ContentHeader>
-      <ScrollArea className="h-[calc(100vh-120px)] md:h-[calc(100vh-180px)]">
+      <ScrollArea className="h-[calc(100vh-140px)] md:h-[calc(100vh-180px)]">
         <div className="pr-4">
           <section className=" mx-5">
             <Typography variant="3xl/extrabold" className="my-2" as="h2">
