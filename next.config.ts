26914ad1f993b5e3b40194a71d84411a1803e9d6
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-07-16 22:23:39 +1200

    feat: enable react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: { webpackMemoryOptimizations: true },
+  experimental: { webpackMemoryOptimizations: true, reactCompiler: true },
 };
 
 export default nextConfig;
