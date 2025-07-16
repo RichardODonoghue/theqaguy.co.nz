e8d84f431462339392ab98b332955ae869123db2
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-07-16 22:24:14 +1200

    feat: blogs page

--- src/app/qa-blog/page.tsx
+++ src/app/qa-blog/page.tsx
@@ -1,5 +1,18 @@
 import { ContentHeader } from '@/components/ui/contentHeader';
+import { getBlogs } from '@/lib/blogs';
+import { BlogCard } from './blogCard';
 
-export default function Blog() {
-  return <ContentHeader>QA_Blog</ContentHeader>;
+export default async function BlogsPage() {
+  const blogsData = await getBlogs();
+
+  const blogs = blogsData.map((blog) => (
+    <BlogCard key={blog.slug} blog={blog} />
+  ));
+
+  return (
+    <>
+      <ContentHeader>QA_Blog</ContentHeader>
+      <div className="grid grid-cols-3">{blogs}</div>
+    </>
+  );
 }
