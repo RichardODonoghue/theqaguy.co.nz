c3c1d7c7ef01579b8b9cd61099d062154b720aee
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-08-11 07:42:18 +1200

    fix: type error in imageUploader

--- src/components/customEditor/imageUploader.tsx
+++ src/components/customEditor/imageUploader.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { CldUploadWidget } from "next-cloudinary";
-import { Button } from "../ui/button";
-import { updateBlogBySlug } from "@/lib/blogs";
+import { CldUploadWidget, CloudinaryUploadWidgetInfo } from 'next-cloudinary';
+import { Button } from '../ui/button';
+import { updateBlogBySlug } from '@/lib/blogs';
 
 interface ImageUploaderProps {
   slug: string;
@@ -14,12 +14,12 @@ interface ImageUploaderProps {
 export const ImageUploader = ({ slug, preset, label }: ImageUploaderProps) => {
   return (
     <CldUploadWidget
-      options={{ sources: ["local"], folder: "theqaguy.co.nz/blog" }}
+      options={{ sources: ['local'], folder: 'theqaguy.co.nz/blog' }}
       uploadPreset={preset}
-      signatureEndpoint={"/api/sign-upload"}
-      onSuccess={async (result, { widget }) => {
-        const publicId: string = result.info.public_id;
-        await updateBlogBySlug(slug, { image: publicId });
+      signatureEndpoint="/api/sign-upload"
+      onSuccess={async (results, { widget }) => {
+        const uploadInfo = results.info as CloudinaryUploadWidgetInfo;
+        await updateBlogBySlug(slug, { image: uploadInfo.public_id });
         widget.close();
       }}
     >
