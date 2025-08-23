0e61540ddb2270ae92bee856a629728bab2ec94d
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-08-23 21:26:03 +1200

    fix: project page tests

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,9 +2,10 @@ import { ReactNode } from 'react';
 
 interface CardProps {
   children: ReactNode;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
   overrides?: string;
+  'data-testid'?: string;
 }
 
 export const Card = ({
@@ -12,10 +13,12 @@ export const Card = ({
   width = 'w-50',
   height = 'h-auto',
   overrides = '',
+  'data-testid': dataTestId,
 }: CardProps) => {
   return (
     <div
       className={`rounded-2xl flex-1 p-2 md:p-4 backdrop-blur-3xl bg-slate-700/30 shadow-2xl mx-5 ${width} ${height} ${overrides}`}
+      data-testid={dataTestId}
     >
       {children}
     </div>
