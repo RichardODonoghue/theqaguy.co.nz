81eebc770010051d28d88651ecbb2ec44df1d4b8
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-08-24 13:30:52 +1200

    chore: upgrade to next 15.5 to fix build issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: 'nodejs',
   matcher: ['/admin/:path*'], // Apply middleware to specific routes
 };
