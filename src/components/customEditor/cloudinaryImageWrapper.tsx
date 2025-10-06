e80506a55ff98086ac71782a37f3ad551296dcc8
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-10-06 21:18:31 +1300

    feat: resizable images (#72)

--- src/components/customEditor/cloudinaryImageWrapper.tsx
+++ src/components/customEditor/cloudinaryImageWrapper.tsx
@@ -1,18 +1,75 @@
 'use client';
 
 import { NodeViewWrapper, ReactNodeViewProps } from '@tiptap/react';
-import { CldImage } from 'next-cloudinary';
-import React from 'react';
+import Image from 'next/image';
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
+import { useState } from 'react';
+import { Button } from '../ui/button';
 
 export const CldImageWrapper = (props: ReactNodeViewProps) => {
+  const [imageSize, setImageSize] = useState<'w-1/3' | 'w-2/3' | 'w-full'>(
+    'w-2/3'
+  );
   // Extract attributes from the node
-  const { src, alt, width, height, float, ...rest } = props.node.attrs;
+  const { src, alt, ...rest } = props.node.attrs;
 
-  const floatClass = float === 'right' ? 'float-right ml-4' : 'float-left mr-4';
+  const handleResize = (size: 'small' | 'medium' | 'large') => {
+    switch (size) {
+      case 'small':
+        setImageSize('w-1/3');
+        break;
+      case 'medium':
+        setImageSize('w-2/3');
+        break;
+      case 'large':
+        setImageSize('w-full');
+        break;
+    }
+    props.updateAttributes({ size: imageSize });
+  };
+
+  console.log(src);
 
   return (
-    <NodeViewWrapper className={`react-component ${floatClass}`}>
-      <CldImage src={src} alt={alt} width={width} height={height} {...rest} />
+    <NodeViewWrapper>
+      <Tooltip>
+        <TooltipTrigger className="w-full">
+          <Image
+            src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/${src}`}
+            width={800}
+            height={800}
+            alt={alt}
+            className={`${imageSize} mx-auto rounded-sm`}
+            {...rest}
+          />
+        </TooltipTrigger>
+        <TooltipContent className="flex flex-row gap-2 p-2">
+          <Button
+            variant="secondary"
+            size="sm"
+            className="hover:bg-accent hover:cursor-pointer"
+            onClick={() => handleResize('small')}
+          >
+            Small
+          </Button>
+          <Button
+            variant="secondary"
+            size="sm"
+            className="hover:bg-accent hover:cursor-pointer"
+            onClick={() => handleResize('medium')}
+          >
+            Medium
+          </Button>
+          <Button
+            variant="secondary"
+            size="sm"
+            className="hover:bg-accent hover:cursor-pointer"
+            onClick={() => handleResize('large')}
+          >
+            Large
+          </Button>
+        </TooltipContent>
+      </Tooltip>
     </NodeViewWrapper>
   );
 };
--- src/components/customEditor/extensions/cloudinaryImage.ts
+++ src/components/customEditor/extensions/cloudinaryImage.ts
@@ -16,8 +16,9 @@ export const CloudinaryImage = Node.create({
       alt: {
         default: '',
       },
-      width: { default: 400 },
-      height: { default: 300 },
+      size: {
+        default: 'w-2/3', // Default size
+      },
     };
   },
   parseHTML() {
@@ -33,7 +34,7 @@ export const CloudinaryImage = Node.create({
     // This is an ugly hack becaue I wasn't able to get the static renderer to properly render react components nested within the content
     // TODO: Fix this shit
 
-    const CLOUDINARY_BASE = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_limit,w_828/f_auto/q_auto/v1/`;
+    const CLOUDINARY_BASE = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_limit,w_1920/f_auto/q_auto/v1/`;
 
     // Use HTMLAttributes.src directly
     const src = HTMLAttributes.src;
@@ -44,7 +45,7 @@ export const CloudinaryImage = Node.create({
       'img',
       mergeAttributes(HTMLAttributes, {
         src: patchedSrc,
-        class: `my-4`,
+        class: `my-4 mx-auto rounded-sm ${HTMLAttributes.size}`,
       }),
     ];
   },
