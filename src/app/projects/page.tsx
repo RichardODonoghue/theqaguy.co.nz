f352879f3d1c93a8000c66a19a38c46b09394ed2
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-10-19 13:51:51 +1300

    feat: extend project page tests

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,40 @@ import { projects } from '@/constants/projects';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { ProjectCard } from './projectCard';
 
+export async function generateMetadata() {
+  return {
+    title: 'TheQAGuy | Projects',
+    description: 'Explore the projects I have worked on',
+    keywords: [
+      'TheQAGuy',
+      'Software Projects',
+      'QA Engineer',
+      'Software Testing',
+      'Quality Assurance',
+      'React.js',
+      'Next.js',
+      'Node.js',
+      'Open Source',
+      'Portfolio',
+      'GitHub',
+      'Project Showcase',
+      'Automation',
+      'Testing Tools',
+      'Web Development',
+      'Software Development',
+      'Tech Projects',
+      'Personal Projects',
+      'Professional Work',
+      'Coding',
+      'Programming',
+      'Tech Stack',
+      'Software Engineering',
+      'Project Management',
+      'Continuous Integration',
+    ],
+  };
+}
+
 export default function Projects() {
   return (
     <>
@@ -12,7 +46,11 @@ export default function Projects() {
       <ScrollArea className="h-full">
         <div className="grid grid-cols-1 mb-20">
           {Object.entries(projects).map(([classification, projects]) => (
-            <div key={classification} className="mx-5">
+            <section
+              aria-labelledby={`${classification}-projects`}
+              key={classification}
+              className="mx-5"
+            >
               <Typography
                 variant="3xl/bold"
                 as="h2"
@@ -25,7 +63,7 @@ export default function Projects() {
                   <ProjectCard key={project.name} project={project} />
                 ))}
               </div>
-            </div>
+            </section>
           ))}
         </div>
       </ScrollArea>
--- tests/e2e/projects.spec.ts
+++ tests/e2e/projects.spec.ts
@@ -1,21 +1,66 @@
 import { test, expect } from '@playwright/test';
 import config from '../../playwright.config';
 import { projects } from '@/constants/projects';
+import { AxeBuilder } from '@axe-core/playwright';
 
 const baseURL = config.use?.baseURL;
 
 test.use({ baseURL: baseURL });
 
 test.describe(() => {
-  test('Can load projects page', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('/projects');
+  });
 
-    await expect(page.getByText('<Projects/>')).toBeVisible();
+  test('Verify Project Page Metadata', async ({ page }) => {
+    expect(
+      await page.getAttribute('title', 'TheQAGuy | Projects')
+    ).toBeDefined();
+
+    expect(await page.getAttribute('meta[name="description"]', 'content')).toBe(
+      'Explore the projects I have worked on'
+    );
+
+    expect(await page.getAttribute('meta[name="keywords"]', 'content')).toBe(
+      [
+        'TheQAGuy',
+        'Software Projects',
+        'QA Engineer',
+        'Software Testing',
+        'Quality Assurance',
+        'React.js',
+        'Next.js',
+        'Node.js',
+        'Open Source',
+        'Portfolio',
+        'GitHub',
+        'Project Showcase',
+        'Automation',
+        'Testing Tools',
+        'Web Development',
+        'Software Development',
+        'Tech Projects',
+        'Personal Projects',
+        'Professional Work',
+        'Coding',
+        'Programming',
+        'Tech Stack',
+        'Software Engineering',
+        'Project Management',
+        'Continuous Integration',
+      ].join(',')
+    );
+  });
+
+  test('Verify Projects Page Content', async ({ page }) => {
+    await expect(page.locator('_react=ContentHeader')).toHaveText(
+      '<Projects/>'
+    );
 
     for (const [category, items] of Object.entries(projects)) {
       await expect(page.getByRole('heading', { name: category })).toBeVisible();
       for (const project of items) {
-        const projectLocator = page.getByTestId(project.name);
+        const projectLocator = page.getByTestId(`project-${project.name}`);
         await expect(
           projectLocator.getByRole('heading', {
             name: project.name,
@@ -30,5 +75,49 @@ test.describe(() => {
         }
       }
     }
+  });
+
+  test('Verify Project Card Modal Content', async ({ page }) => {
+    // Grab project data for use in test.
+    const targetProject = projects.Software.find(
+      (project) => project.name === 'theqaguy.co.nz'
+    );
+    if (!targetProject) {
+      throw new Error('Project "theqaguy.co.nz" not found in test data.');
+    }
+
+    const projectCard = page.getByTestId('project-theqaguy.co.nz');
+
+    await projectCard.getByRole('button').click();
+
+    const dialog = page.getByRole('dialog');
+    await expect(dialog).toBeVisible();
+
+    await expect(
+      dialog.getByRole('heading', { name: 'theqaguy.co.nz', exact: true })
+    ).toBeVisible();
+
+    await expect(dialog.getByText(targetProject.description)).toBeVisible();
+
+    await expect(
+      dialog.getByText(`Development Stage: ${targetProject.status}`)
+    ).toBeVisible();
+
+    await expect(dialog.getByText(targetProject.status)).toHaveClass(
+      targetProject.status === 'Development'
+        ? 'text-yellow-400'
+        : 'text-green-400'
+    );
+
+    if (targetProject.technologies) {
+      for (const tech of targetProject.technologies) {
+        await expect(dialog.getByText(tech)).toBeVisible();
+      }
+    }
+  });
+
+  test('Accessibility Audit', async ({ page }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
