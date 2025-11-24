8d9c18da1f105d96a5a41fe974fe0db9c3b42180
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-11-24 16:38:54 +1300

    feat: move adminBlogPage tests to use PCOM

--- playwright/adminBlogTable.spec.ts
+++ playwright/adminBlogTable.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import config from '../playwright.config';
+import { AdminBlogPage } from './pcoms/adminBlogs';
 import AxeBuilder from '@axe-core/playwright';
 import { testBlogs } from '@/constants/testBlogs';
 
@@ -8,8 +9,11 @@ const baseURL = config.use?.baseURL as string;
 test.use({ baseURL: baseURL });
 
 test.describe('Admin Blog Table Page', () => {
+  let adminBlogPage: AdminBlogPage;
+
   test.beforeEach(async ({ page }) => {
-    await page.goto('/admin/blog');
+    adminBlogPage = new AdminBlogPage(page);
+    await adminBlogPage.goto();
   });
 
   test('Verify Admin Blog Table Page Metadata', async ({ page }) => {
@@ -18,50 +22,38 @@ test.describe('Admin Blog Table Page', () => {
     ).toBeDefined();
   });
 
-  test('Verify Admin Blog Table Content', async ({ page }) => {
-    const blogTable = page.getByTestId('blog-table');
-    const blogRows = blogTable.locator('tbody tr');
+  test('Verify Admin Blog Table Content', async () => {
+    const blogRows = adminBlogPage.table.locator('tbody tr');
 
-    await expect(blogTable).toBeVisible();
+    await expect(adminBlogPage.table).toBeVisible();
     await expect(blogRows).toHaveCount(testBlogs.length);
   });
 
   testBlogs.forEach((blog) => {
-    test(`Verify Table Row for Blog: ${blog.slug}`, async ({ page }) => {
-      const blogTable = page.getByTestId('blog-table');
-      const blogRow = blogTable
-        .locator('tbody tr')
-        .filter({ hasText: blog.slug });
-
-      const blogSlugCol = blogRow.locator('td').nth(0);
-      const blogTitleCol = blogRow.locator('td').nth(1);
-      const blogSummaryCol = blogRow.locator('td').nth(2);
-      const blogTagsCol = blogRow.locator('td').nth(3);
-      const blogPublishedCol = blogRow.locator('td').nth(4);
-      const blogCreatedAtCol = blogRow.locator('td').nth(5);
+    test(`Verify Table Row for Blog: ${blog.slug}`, async () => {
+      const { slug, title, summary, tags, published, createdAt } =
+        await adminBlogPage.setTableRow(blog.slug);
+      if (!adminBlogPage.tableRow) {
+        throw new Error(`Table row for blog with slug ${blog.slug} not found`);
+      }
 
       // await expect(blogSlugCol).toContainText(blog.slug);
-      await expect(blogSlugCol.getByRole('link')).toHaveAttribute(
+      await expect(slug.getByRole('link')).toHaveAttribute(
         'href',
         `/admin/blog/${blog.slug}`
       );
-      await expect(blogTitleCol).toContainText(blog.title);
-      await expect(blogSummaryCol).toContainText(blog.summary);
-      await expect(blogTagsCol).toContainText(blog.tags.join(','));
-      await expect(blogPublishedCol).toContainText(
-        blog.published ? 'true' : 'false'
-      );
-      await expect(blogCreatedAtCol).toContainText(
-        blog.createdAt.toISOString()
-      );
+      await expect(title).toContainText(blog.title);
+      await expect(summary).toContainText(blog.summary);
+      await expect(tags).toContainText(blog.tags.join(','));
+      await expect(published).toContainText(blog.published ? 'true' : 'false');
+      await expect(createdAt).toContainText(blog.createdAt.toISOString());
     });
   });
 
-  test('Verify New Blog Button Navigation', async ({ page }) => {
-    const newBlogButton = page.getByRole('link', { name: 'New Blog' });
-    await expect(newBlogButton).toBeVisible();
-    await newBlogButton.click();
-    await expect(page).toHaveURL(/\/admin\/blog\/new$/);
+  test('Verify New Blog Button Navigation', async () => {
+    await expect(adminBlogPage.newBlogButton).toBeVisible();
+    await adminBlogPage.newBlogButton.click();
+    await expect(adminBlogPage.page).toHaveURL(/\/admin\/blog\/new$/);
   });
 
   test('Accessibility Audit', async ({ page }) => {
