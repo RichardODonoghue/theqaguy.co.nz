b0c20f9a1517f9bb2dc6b131cf9635e6401f51e7
Richard O'Donoghue <richard.odonoghue.nz@gmail.com>
2025-12-24 21:30:42 +1300

    feat: add logging and metrics

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,16 +1,45 @@
-import { registerOTel, OTLPHttpJsonTraceExporter } from '@vercel/otel';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from '@opentelemetry/semantic-conventions/incubating';
+import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR); // set to DEBUG when troubleshooting
-
 export function register() {
-  registerOTel({
-    serviceName: process.env.OTEL_SERVICE_NAME || 'theqaguy.co.nz',
-    attributes: {
-      'deployment.environment': process.env.OTEL_DEPLOYMENT_ENV || 'local',
-    },
-    traceExporter: new OTLPHttpJsonTraceExporter({
-      url: process.env.OTEL_ENDPOINT,
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+  const sdk = new NodeSDK({
+    resource: resourceFromAttributes({
+      [ATTR_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME || 'theqaguy.co.nz',
+      [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]:
+        process.env.OTEL_DEPLOYMENT_ENV || 'local',
     }),
+    traceExporter: new OTLPTraceExporter({
+      url: `${process.env.OTEL_ENDPOINT}/v1/traces`,
+    }),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new OTLPMetricExporter({
+        url: `${process.env.OTEL_ENDPOINT}/v1/metrics`,
+      }),
+    }),
+    logRecordProcessor: new SimpleLogRecordProcessor(
+      new OTLPLogExporter({
+        url: `${process.env.OTEL_ENDPOINT}/v1/logs`,
+      })
+    ),
+    instrumentations: [
+      getNodeAutoInstrumentations({
+        '@opentelemetry/instrumentation-winston': {
+          enabled: true,
+        },
+      }),
+    ],
   });
+
+  sdk.start();
 }
